91946b8f0ba299cebff1299ce3de4dcafb6746ee
Anton Maminov <anton.linux@gmail.com>
2024-09-28 15:26:20 +0300

    allow input as argument

--- src/ltp_cli.cr
+++ src/ltp_cli.cr
@@ -1,5 +1,7 @@
 require "./ltp"
 
-if input = STDIN.gets
-  LTP.print(input.chomp) if input && !input.empty?
+if input = ARGV[0]?
+  LTP.print(input.chomp) if input && !input.chomp.empty?
+elsif input = STDIN.gets
+  LTP.print(input.chomp) if input && !input.chomp.empty?
 end
